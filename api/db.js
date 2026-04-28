01d5269bbd6625f44ec573a1432802a532ad54ea
Rafael Sartori Milanez <rafaelsartorimilanez1@gmail.com>
2026-04-28 16:39:37 -0300

    modificoes no db.js

--- api/db.js
+++ api/db.js
@@ -1,5 +1,12 @@
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL);
+// cria conexão usando as variáveis do Railway
+const connection = await mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 
 export default connection;
